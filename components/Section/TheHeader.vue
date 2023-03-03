23d4beb112fa54855f02c9275c6364b87d32efc2
usamahamid <usamahamid@esparkconsultants.com>
2023-03-03 17:22:02 +0500

    add header

--- components/Section/TheHeader.vue
+++ components/Section/TheHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <TopBar :offerCode="offerCode" />
     <div class="container-fluid">
       <nav class="d-flex justify-content-between">
         <div>
@@ -15,29 +16,68 @@
   </header>
 </template>
 <script>
+import TopBar from './Elements/HeaderElements/TopBar.vue'
 export default {
   name: 'TheHeader',
+  props: ['offerCode', 'stopScrollWork'],
+  components: { TopBar },
+  data() {
+    return {
+      isSearchbarOpen: false,
+      headerDown: false,
+      scrollTopDiff: 0,
+      scrollUp: false,
+      mobileView: false,
+      scrollDown: false,
+    }
+  },
+  created() {
+    if (process.client) {
+      window.addEventListener('scroll', this.handleScroll)
+      window.addEventListener('resize', this.getWindowWidth)
+    }
+  },
+  destroyed() {
+    if (process.client) {
+      window.removeEventListener('scroll', this.handleScroll)
+      window.removeEventListener('resize', this.getWindowWidth)
+    }
+  },
+  methods: {
+    toggleSearchbar() {
+      this.isSearchbarOpen = !this.isSearchbarOpen
+    },
+    getWindowWidth() {
+      if (process.client) {
+        if (screen.availWidth < 1200) this.mobileView = true
+        else this.mobileView = false
+      }
+    },
+    handleScroll(event) {
+      if (process.client) {
+        if (!this.stopScrollWork) {
+          if (window.scrollY > 0) {
+            if (!this.headerDown) {
+              this.headerDown = true
+              this.scrollDown = false
+              this.scrollUp = false
+            }
+            if (window.scrollY > this.scrollTopDiff) {
+              this.scrollDown = true
+              this.scrollUp = false
+            } else {
+              this.scrollUp = true
+            }
+            this.scrollTopDiff = window.scrollY
+          } else if (window.scrollY === 0) {
+            this.headerDown = false
+            this.scrollDown = false
+            this.scrollUp = false
+          }
+        }
+      }
+    },
+  },
 }
 </script>
-<style scoped>
-header {
-  background-color: white;
-  height: 8vh;
-  /* box-shadow: 0 10px 25px rgb(167, 165, 165); */
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-}
-nav a {
-  color: green;
-  font-weight: bold;
-  margin-right: 2rem;
-  display: inline-block;
-  text-decoration: none;
-  transition: all 0.3s ease;
-}
-nav a:hover {
-  transform: rotateZ(-15deg);
-  text-shadow: 1px 1px 4px green;
-}
-</style>
+<style scoped></style>
